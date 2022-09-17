92986949f80835e8f77ab5175089d3f872d393d3
chewzzz <chewziqing@gmail.com>
2022-09-17 22:24:09 +0800

    Complete counter

--- counter/counter.js
+++ counter/counter.js
@@ -12,12 +12,39 @@ function addNumber() {
   const container = document.createElement("div");
   const text = document.createElement("p");
 
-  text.innerHTML = "<b>" + displayNumber.value + "</b>";
+  text.innerHTML = "<b>" + displayNumber.textContent + "</b>";
 
   container.insertAdjacentElement("beforeend", text);
-  container.classList.toogle("number-box");
+  container.classList.add("number-box");
   result.insertAdjacentElement("beforeend", container);
-  displayNumber.textContent = "";
+
+}
+
+function increase() {
+  displayNumber.textContent = parseInt(displayNumber.textContent) + 1;
+  displayNumber.style.color = randomColor();
+}
+
+function decrease() {
+  displayNumber.textContent = parseInt(displayNumber.textContent) - 1;
+  displayNumber.style.color = randomColor();
+}
+
+function clearNum() {
+  displayNumber.textContent = 0;
+  displayNumber.style.color = randomColor();
+
+  result.textContent = "";
+}
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 255) + 1;
+  const g = Math.floor(Math.random() * 255) + 1;
+  const b = Math.floor(Math.random() * 255) + 1;
+
+  const color = "rgb(" + r + "," + g + "," + b + ")";
+  return color;
 }
 
 save.addEventListener("click", addNumber);
+increment.addEventListener("click", increase); decrement.addEventListener("click", decrease); clear.addEventListener("click", clearNum);
