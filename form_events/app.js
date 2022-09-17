a52db1c99e381a3acde5eed917ba12544912b33b
chewzzz <chewziqing@gmail.com>
2022-09-17 23:10:01 +0800

    Complete form_events

--- form_events/app.js
+++ form_events/app.js
@@ -15,11 +15,28 @@ tweetForm.addEventListener('submit', function (e) {
 });
 
 const addTweet = (username, tweet) => {
-    const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
-    tweetsContainer.append(newTweet);
+    const smallContainer = document.createElement("div");
+    smallContainer.classList.add("singleTweet");
+    smallContainer.classList.add("col");
+    //smallContainer.classList.add("col-md-6");
+
+    // username
+    const userNameP = document.createElement("p");
+
+    userNameP.innerHTML = "<b>" + username + "</b>";
+
+    smallContainer.insertAdjacentElement("beforeend", userNameP);
+
+    // linebreak between username and tweet
+    const lineBreak = document.createElement("br");
+    smallContainer.insertAdjacentElement("beforeend", lineBreak);
+
+    // user tweet
+    const tweetP = document.createElement("p");
+    tweetP.append(tweet);
+    smallContainer.insertAdjacentElement("beforeend", tweetP);
+
+    // single tweet container
+    tweetsContainer.insertAdjacentElement("beforeend", smallContainer);
 }
 
