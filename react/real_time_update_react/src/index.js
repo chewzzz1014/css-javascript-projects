80a8107b3a12d0dbbeec918baba44febeb18affb
chewzzz <chewziqing@gmail.com>
2022-10-07 14:52:38 +0800

    Add js selection in return stmt of react component

--- react/real_time_update_react/src/index.js
+++ react/real_time_update_react/src/index.js
@@ -40,6 +40,63 @@ root.render(
   <ControlledInput />, document.getElementById("root")
 );
 
+
+// version two: display button based on user input.
+// when the page first loads, render the submit button, buttonOne, to the page. Then, when a user enters their age and clicks the button, render a different button based on the age. If a user enters a number less than 18, render buttonThree. If a user enters a number greater than or equal to 18, render buttonTwo.
+
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state = {
+      input: "",
+      userAge: ""
+    };
+    // Change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type='number'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+        {/* Change code below this line */}
+        {this.state.userAge == "" ? buttonOne :
+          this.state.userAge >= 18 ? buttonTwo : buttonThree}
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
