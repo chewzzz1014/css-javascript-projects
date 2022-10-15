3fc49a11e189ecdf35d8cc96444243b2397bf849
chewzzz <chewziqing@gmail.com>
2022-10-15 12:57:17 +0800

    Add state

--- freeCodeCamp/react/react-demo/src/App.js
+++ freeCodeCamp/react/react-demo/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Hello from './sayHello';
 import Tweet from './tweet';
 // for styling
@@ -7,17 +7,49 @@ import './App.css';
 
 // component
 function App() {
+
+  // // state
+  // const [isRed, setRed] = useState(false);
+  // const [count, setCount] = useState(0);
+
+  // const increment = () => {
+  //   setCount(count + 1);
+
+  //   // toggle isRed
+  //   setRed(!isRed);
+
+  const [users, setUsers] = useState([
+    { name: "chewzzz", msg: "first post! so excitedddddd" },
+    { name: "Dokki", msg: "so am I!!!!!!" },
+    { name: "huhuMonster", msg: "wow first time to see this..." },
+    { name: "weeew", msg: "something wrong here" },
+    { name: "mmmum", msg: "react tweet" },
+    { name: "mommyBoi", msg: "heyyyyyy mate nice to meet you" },
+    { name: "monster", msg: "yoyoyyyyyy" },
+  ]);
+
+
   return (
     <div className='app'>
       {/* <h1>This is an App Component</h1>
       <Hello /> */}
-      <Tweet name="chewzzz" msg="first post! so excitedddddd" />
+      {/* <Tweet name="chewzzz" msg="first post! so excitedddddd" />
       <Tweet name="Dokki" msg="so am I!!!!!!" />
       <Tweet name="huhuMonster" msg="wow first time to see this..." />
       <Tweet name="weeew" msg="something wrong here" />
       <Tweet name="mmmum" msg="react tweet" />
       <Tweet name="mommyBoi" msg="heyyyyyy mate nice to meet you" />
-      <Tweet name="monster" msg="yoyoyyyyyy" />
+      <Tweet name="monster" msg="yoyoyyyyyy" /> */}
+
+
+      {
+        users.map(user =>
+          <Tweet {...user} />)
+      }
+
+      {/* <h1 className={isRed ? 'red' : ''}>Change My Color</h1>
+    <button onClick={increment}>Increment</button>
+    <h1>{count}</h1> */}
     </div>
   );
 }
