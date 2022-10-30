d7acac1527c6861bf1098184d89b97a995e44a6e
chewzzz <chewziqing@gmail.com>
2022-10-30 17:16:30 +0800

    Add hover effect

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,12 +1,14 @@
 const canvas = document.querySelector("#canvas");
-const sketchBoxes = Array.from(document.querySelectorAll("grid-item"));
+const boxes = document.querySelectorAll("#canvas > div");
 const btnClear = document.querySelector(".btn-clear");
 const btnEraser = document.querySelector(".btn-eraser");
 const btnRainbow = document.querySelector(".btn-rainbow");
-const btnRandomColor = document.querySelector(".btn-random-clear");
+const btnRandomColor = document.querySelector(".btn-random-color");
 const btnSmall = document.querySelector(".btn-small");
 const btnMedium = document.querySelector(".btn-medium");
 const btnLarge = document.querySelector(".btn-large");
+
+let currentStatus = 'RANDOM';
 
 const size = {
     small: {
@@ -23,21 +25,64 @@ const size = {
     },
 };
 
-let currentStatus = 'RANDOM';
+createCanvas(size.medium.numBoxes);
+paint();
 
-sketchBoxes.forEach(function (ele) {
-    ele.addEventListener("click", () => {
-        if (currentStatus === 'RANDOM') {
 
-        } else if (currentStatus === 'RAINBOW') {
 
-        } else if (currentStatus === 'CLEAR') {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function changeCanvasSize(numBox, sizeSide) {
+    canvas.style.gridTemplateRows = `repeat(${numBox}, ${sizeSide}px)`;
+    canvas.style.gridTemplateColumns = `repeat(${numBox}, ${sizeSide}px)`;
+}
 
-        } else if (currentStatus === 'ERASER') {
-
+function createCanvas(n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            const div = document.createElement("div");
+            div.classList.add("grid-item");
+            canvas.insertAdjacentElement("beforeend", div);
         }
+    }
+}
+
+function clearBoxes() {
+    canvas.innerHTML = "";
+};
+
+function randomColor() {
+    const color = Math.floor(Math.random() * 256);
+    return color;
+}
+
+function getColor() {
+    const r = randomColor();
+    const g = randomColor();
+    const b = randomColor();
+    if (r <= 80 && g <= 80 || r <= 80 && b <= 80 || g <= 80 && b <= 80 || (r <= 50 || g <= 50 || b <= 50))
+        isDark = true;
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+function paint() {
+    boxes.forEach((ele) => {
+        const selectedBox = ele;
+        selectedBox.addEventListener("mouseenter", (e) => {
+            if (currentStatus === 'RANDOM') {
+                alert("yayy")
+                e.target.style.backgroundColor = getColor();
+                e.target.style.opacity = 1;
+            } else if (currentStatus === 'RAINBOW') {
+
+            } else if (currentStatus === 'CLEAR') {
+
+            } else if (currentStatus === 'ERASER') {
+
+            }
+        })
     })
-})
+}
+
 
 btnSmall.addEventListener("click", () => {
     clearBoxes();
@@ -58,52 +103,21 @@ btnLarge.addEventListener("click", () => {
 })
 
 btnClear.addEventListener("click", () => {
+    alert("clear")
     currentStatus = "CLEAR";
 })
 
 btnRainbow.addEventListener("click", () => {
+    alert("rainbow")
     currentStatus = "RAINBOW";
 })
 
 btnRandomColor.addEventListener("click", () => {
+    alert("random")
     currentStatus = "RANDOM";
 })
 
 btnEraser.addEventListener("click", () => {
+    alert("eraser")
     currentStatus = 'ERASER';
 })
-
-const changeCanvasSize = (numBox, sizeSide) => {
-    canvas.style.gridTemplateRows = `repeat(${numBox}, ${sizeSide}px)`;
-    canvas.style.gridTemplateColumns = `repeat(${numBox}, ${sizeSide}px)`;
-}
-
-const createCanvas = (n) => {
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            const div = document.createElement("div");
-            div.classList.add("grid-item");
-            canvas.insertAdjacentElement("beforeend", div);
-        }
-    }
-}
-
-const clearBoxes = () => {
-    canvas.innerHTML = "";
-}
-
-function randomColor() {
-    const color = Math.floor(Math.random() * 256);
-    return color;
-}
-
-function getColor() {
-    const r = randomColor();
-    const g = randomColor();
-    const b = randomColor();
-    if (r <= 80 && g <= 80 || r <= 80 && b <= 80 || g <= 80 && b <= 80 || (r <= 50 || g <= 50 || b <= 50))
-        isDark = true;
-    return "rgb(" + r + "," + g + "," + b + ")";
-}
-
-createCanvas(30);
